aae263d0f97f1ab95fa1ee9b60fdbcbd847efba1
DoubleStyx <79298541+DoubleStyx@users.noreply.github.com>
2022-08-10 23:19:32 -0500

    Add files via upload

---
+++ DisableFXAA.cs
@@ -0,0 +1,53 @@
+﻿using HarmonyLib;
+using NeosModLoader;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+namespace DisableFXAA
+{
+    public class DisableFXAA : NeosMod
+    {
+        public override string Name => "DisableFXAA";
+        public override string Author => "DoubleStyx";
+        public override string Version => "1.0.0";
+        public override string Link => "https://github.com/DoubleStyx/DisableFXAA";
+
+        private static ModConfiguration config;
+
+        [AutoRegisterConfigKey]
+        private static ModConfigurationKey<bool> fxaaEnabled = new ModConfigurationKey<bool>("Enable FXAA", "", () => false);
+
+        public override void OnEngineInit()
+        {
+            Harmony harmony = new Harmony("net.DoubleStyx.DisableFXAA");
+            harmony.PatchAll();
+
+            config = GetConfiguration();
+            ModConfiguration.OnAnyConfigurationChanged += OnConfigurationChanged;
+        }
+
+        private void OnConfigurationChanged(ConfigurationChangedEvent @event)
+        {
+            Camera main = Camera.main;
+            SetFxaaActive(main, config.GetValue(fxaaEnabled));
+        }
+
+        public static void SetFxaaActive(Camera camera, bool state)
+        {
+            PostProcessLayer layer = camera.GetComponent<PostProcessLayer>();
+            layer.antialiasingMode = state
+                ? PostProcessLayer.Antialiasing.FastApproximateAntialiasing
+                : PostProcessLayer.Antialiasing.None;
+        }
+
+        [HarmonyPatch(typeof(CameraInitializer), "SetupPostProcessing")]
+        static class Patch
+        {
+            static void Postfix(Camera c, bool motionBlur, bool screenspaceReflections, bool mainCamera = false, bool vr = false, bool singleCapture = false)
+            {
+                Camera main = Camera.main;
+                SetFxaaActive(main, config.GetValue(fxaaEnabled));
+            }
+        }
+    }
+}
